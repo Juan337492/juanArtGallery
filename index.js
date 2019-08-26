8628ea5456379effa43f3ccba0e1495c5fe4ec67
Juan Rodriguez <Juan337492@yahoo.com>
2019-08-26 01:10:29 -0400

    -fixed menu js only happens once now

--- index.js
+++ index.js
@@ -11,18 +11,4 @@ slowmo.addEventListener('click', function(e){
 	this.classList.toggle('is-slowmo');
 });
 
-/* Onload demo - dirty timeout */
-let clickEvent = new Event('click');
 
-window.addEventListener('load', function(e) {
-	slowmo.dispatchEvent(clickEvent);
-	burger.dispatchEvent(clickEvent);
-	
-	setTimeout(function(){
-		burger.dispatchEvent(clickEvent);
-		
-		setTimeout(function(){
-			slowmo.dispatchEvent(clickEvent);
-		}, 3500);
-	}, 5500);
-});
